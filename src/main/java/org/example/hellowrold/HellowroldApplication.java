598dac82a295c030f8e2d4d44a673cfade1b91bf
youbme <aayushgurung1993@gmail.com>
2025-03-19 16:26:48 +0545

    Initial commit

---
+++ src/main/java/org/example/hellowrold/HellowroldApplication.java
@@ -0,0 +1,16 @@
+package org.example.hellowrold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowroldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowroldApplication.class, args);
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+
+}
