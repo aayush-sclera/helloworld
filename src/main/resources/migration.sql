c716b71de2bfac7f8a0f25a0c1dfbecf351a4376
aayush-sclera <aayush.gurung@sclera.com>
2025-03-24 11:53:11 +0545

    Update migration.sql

--- src/main/resources/migration.sql
+++ src/main/resources/migration.sql
@@ -1,10 +1,10 @@
 
 Use cicdpipeline;
 -- Create the 'filters' table
-CREATE TABLE filters (
+CREATE TABLE chomu (
                          id varchar(50),
                          name varchar(50)
 );
 
 -- Add a new column 'phone_number' to the 'demo' table (if it exists)
-ALTER TABLE demo ADD COLUMN phone_number varchar(50);
+ALTER TABLE demo ADD COLUMN diddy varchar(50);
